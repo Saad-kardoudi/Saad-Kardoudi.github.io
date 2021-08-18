97d214d8b1286484550fc390afb048da7220db4c
Saad-kardoudi <saad.kardoudi.24@gmail.com>
2021-08-18 20:35:09 +0100

    lil tewk

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,8 +6,10 @@ const About = () => {
   const folderHandler = () => {
     let folders = document.getElementsByClassName("folder-holder");
     let left = folders[0].offsetLeft + folders[0].offsetWidth;
+    folders[0].style.top = "50px";
     for (let index = 1; index < folders.length; index++) {
       folders[index].style.left = `${left}px`;
+      folders[index].style.top = "50px";
       left = left + folders[index].offsetWidth;
     }
   };
