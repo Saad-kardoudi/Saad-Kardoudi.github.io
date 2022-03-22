c9db21464ed4306328fcd5c8363293382b63dccc
Saad-kardoudi <saad.kardoudi.24@gmail.com>
2022-03-22 21:35:18 +0100

    fix dashbord

--- public/files/SandBox/Dashbored/app.js
+++ public/files/SandBox/Dashbored/app.js
@@ -1,44 +1,30 @@
-const side_marg = () => {
-  let wide = document.querySelector(".side-bar").offsetWidth;
-  document.querySelector(".main").style.marginLeft = `${wide}px`;
-};
-const right_side_marg = () => {
-  let wide = document.querySelector(".right-sidebar").offsetWidth;
-  document.querySelector(".main").style.marginRight = `${wide + 20}px`;
-};
+let wide;
 const side_marg_icon = () => {
   let wide = document.querySelector(".bx").offsetWidth;
   document.querySelector(".side-bar").style.width = `${wide}px`;
-  document.querySelector(".main").style.marginLeft = `${wide + 40}px`;
 };
 const open_side = () => {
-  if (document.querySelector(".side-bar").style.width != `auto`) {
-    document.querySelector(".side-bar").style.width = `auto`;
-    side_marg();
+  if (
+    document.querySelector(".side-bar").style.width ==
+    `${document.querySelector(".bx").offsetWidth}px`
+  ) {
+    //document.querySelector(".side-bar").style.width = ` auto `;
+    //wide = document.querySelector(".side-bar").offsetWidth;
+    document.querySelector(".side-bar").style.width = `${wide}px`;
   } else {
     side_marg_icon();
   }
 };
-function setProgress() {
-  var circleT = document.getElementsByClassName("progress-ring__circle");
-  for (let index = 0; index < circleT.length; index++) {
-    const circle = circleT[index];
-    var percent = parseInt(circle.dataset.percent);
-    var radius = circle.r.baseVal.value;
-    var circumference = radius * 2 * Math.PI;
-    circle.style.strokeDasharray = `${circumference} ${circumference}`;
-    circle.style.strokeDashoffset = `${circumference}`;
-    const offset = circumference - (percent / 100) * circumference;
-    circle.style.strokeDashoffset = offset;
-  }
-}
 
-//const input = document.querySelector("input");
-//setProgress(input.value);
+const hi = () => {
+  console.log("hii");
+  document.querySelector(".side-bar").style.width = ` auto `;
+  wide = document.querySelector(".side-bar").offsetWidth;
+};
+
 window.onload = () => {
   setTimeout(() => {
-    setProgress();
+    hi();
     side_marg_icon();
-    right_side_marg();
   }, 50);
 };
